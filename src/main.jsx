c0ea6a5474eac90660a2e57db9993f9fe7180c91
jvitormcs <vitormouracs@gmail.com>
2022-12-05 18:50:30 -0300

    Ajuste no menu

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import GlobalStyles from './Global'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import QuemSomos from './screens/QuemSomos';
+
+const router = createBrowserRouter([
+  {
+    path: "/teste",
+    element: <QuemSomos />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyles />
+    <RouterProvider/>
     <App />
   </React.StrictMode>
 )
